444eade765323567448afb67e4c0d603f0cdd124
Peso4ekkkk <timur.sibgatov0233@mail.ru>
2023-07-09 21:51:02 +0300

    Styled forms

---
+++ project 1/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html, css}"],
+    theme: {
+      extend: {
+        fontFamily:{
+          Montserrat:["Montserrat","sans-serif"],
+        }
+      },
+      screen:{
+        lg: "1280px",
+        md: "792px",
+        sm: "320px",
+      },
+      colors:{
+        black:'#27272A',
+        greyDark:'#696969',
+        greyWhite:'#27272A',
+        white:'#FCFBFA',
+        lavender:'#7E3AF2',
+        red:'#E02424'
+      }
+
+    },
+    plugins: [],
+  }
