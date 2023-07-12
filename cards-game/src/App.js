fb768cf57628c4d24ae3a0473256491341637960
Сибгатов Тимур <timur.sibgatov0233@mail.ru>
2023-07-12 18:49:34 +0300

    Dima lox

--- cards-game/src/App.js
+++ cards-game/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import SingleCard from "./components/SingleCard";
 import bear from "./image/bear.png";
@@ -11,21 +11,21 @@ import squirrel from "./image/squirrel.png";
 import zebra from "./image/zebra.png";
 
 const cardImages = [
-  bear,
-  cow,
-  elephant,
-  kangaroo,
-  mouse,
-  snail,
-  squirrel,
-  zebra,
+  { bear, matched: false },
+  { cow, matched: false },
+  { elephant, matched: false },
+  { kangaroo, matched: false },
+  { mouse, matched: false },
+  { snail, matched: false },
+  { squirrel, matched: false },
+  { zebra, matched: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
-  const [choiseOne, setChoiceOne] = useState(null);
-  const [choiseTwo, setChoiceTwo] = useState(null);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -36,27 +36,53 @@ function App() {
   };
 
   const handleChoice = (card) => {
-    choiseOne ? setChoiceTwo(card) : setChoiceOne(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
-  console.log("choiseOne ", choiseOne);
-  console.log("choiseTwo ", choiseTwo);
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.img === choiceTwo.img) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.img === choiceOne.img) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   return (
-    <div className="App" class="bg-black  h-[100vh]">
-      <h1 class="flex justify-center items-cener text-[2.5rem]  text-white">
+    <div className="App bg-greyDark  h-[100vh] w-[100%]">
+      <h1 className="flex justify-center items-cener text-[2.5rem]  text-white">
         Find a pair
       </h1>
-      <div class="flex justify-center items-center">
+      <div className="flex justify-center items-center">
         <button
           onClick={shuffleCards}
-          class="  border-[2px] rounded-[10px] bg-none cursor-pointer border-white text-lavender "
+          className="  border-[2px] rounded-[10px] bg-none cursor-pointer border-white text-lavender "
         >
           New game
         </button>
       </div>
-      <div className="card-grid" class="mt-[40px] grid grid-cols-4 gap-1">
+      <div className="card-grid mt-[50px] ml-[450px] grid grid-cols-4 gap-0.5 w-[60%]">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
--- cards-game/src/components/SingleCard.jsx
+++ cards-game/src/components/SingleCard.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "../index.css";
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   };
   return (
-    <div className="card" class="relative" key={card.id}>
-      <div class="border-[2px] border-white">
-        <img className="front bg-lavender" src={card.img} alt="card front" />
-        <div className="back" onClick={handleClick}>
+    <div className="card" key={card.id}>
+      <div className={flipped ? "flipped" : ""}></div>
+      <div className="border-[1px] border-white w-[96px] h-[96px]">
+        <div
+          className="back bg-lavender pl-[2.75rem] pb-[2.75rem] relative "
+          onClick={handleClick}
+        >
           ?
         </div>
+        <img
+          className="front bg-lavender block w-[100%] relative"
+          src={card.img}
+          alt="card front"
+        />
       </div>
     </div>
   );
