ff1f7882a311f3af7f68b541683fd423ee34a565
Peso4ekkkk <timur.sibgatov0233@mail.ru>
2023-07-13 23:56:13 +0300

    Finish commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 const API_KEY = '6368168625a9116481d42369235bc9f0';
 
@@ -15,11 +14,11 @@ function WeatherApp() {
 
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold mb-4 flex justify-center text-white">Weather App</h1>
+      <h1 className="text-4xl font-bold mb-4 flex justify-center text-white">Weather App</h1>
       <AddCityForm onAddCity={handleAddCity} />
-      <ul className="mt-4 text-white text-xl">
+      <ul className="mt-4 text-blue-500 font-bold text-3xl flex justify-center">
         {cities.map((city) => (
-          <CityWeather key={city} city={city} onRemoveCity={handleRemoveCity} />
+          <CityWeather className="flex" key={city} city={city} onRemoveCity={handleRemoveCity} />
         ))}
       </ul>
     </div>
@@ -36,18 +35,21 @@ function AddCityForm({ onAddCity }) {
   };
 
   return (
-    <form className='flex justify-end' onSubmit={handleSubmit}>
-      <input
+    <div className='flex justify-center'>
+        <form onSubmit={handleSubmit}>
+      <input 
         type="text"
-        placeholder="Enter city"
+        placeholder="Введите город"
         value={city}
         onChange={(e) => setCity(e.target.value)}
-        className="border px-2 py-1 rounded-10px mr-2  "
+        className="border rounded px-2 py-1 "
       />
-      <button type="submit" className="bg-blue-500 text-white px-2 py-1 rounded flex justify-end focus:bg-purple-500">
+      <button type="submit" className="bg-blue-500 text-white px-2 py-1.5 rounded focus:bg-purple-500 ">
         Add City
       </button>
     </form>
+    </div>
+  
   );
 }
 
@@ -65,21 +67,22 @@ function CityWeather({ city, onRemoveCity }) {
   }, [city]);
 
   return (
-    <li className="flex justify-between items-center mb-2">
-      <div>
+    <li className="flex justify-center items-center mb-2 bg-gray-300 p-[3rem] backdrop-opacity-0 mr-[10px]">
+      <div className='flex'>
+        <div>
         <div>{city}</div>
         {weather && (
           <div>
             {Math.round(weather.main.temp - 273.15)}°C, {weather.weather[0].main}
           </div>
         )}
-      </div>
-      <button
-        className="bg-purple-300 text-white px-2 py-1 rounded"
-        onClick={() => onRemoveCity(city)}
-      >
-        Remove
+        </div>
+        <button
+        className="bg-red-500 text-white px-[1rem] py-[1rem] rounded ml-[2rem]"
+        onClick={() => onRemoveCity(city)}>
+        x
       </button>
+      </div>
     </li>
   );
 }
